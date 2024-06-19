2fa8eedd3f297b2f584f563f5699a4a43a47100c
Hilmar09 <hil.cris09@gmail.com>
2024-06-19 14:20:40 +0200

    TDD

--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
@@ -28,6 +30,77 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 5.0);
 
     }
+    @Test
+    void calculatePriceForMagicCards_green() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 4.40);
+
+    }
+    @Test
+    void calculatePriceForMagicCards_black() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Black", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 6.8);
+
+    }
+    //TODO verificar esta funcion, ya que el valor 0,10 y no null.
+    @Test
+    void calculatePriceForPets_blueFish() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "blue", null, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), null);
+
+    }
+
+    //TODO verificar esta funcion ya que el valor es 100 y no null.
+    @Test
+    void calculatePriceForPets_GoldFish() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "Gold", null, null, null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), null);
+
+    }
+    @Test
+    void calculatePriceForMagicCards_blackLotus() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "null", null, "Magic: The Gathering - Black Lotus", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 40000.0);
+
+    }
+    //TODO colocar el nombre en el default.
+    @Test
+    void calculatePriceForMagicCards_brown() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "brown", null, "Magic: The Gathering ", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2.0);
+
+    }
+
 
     @Test
     void calculatePriceForWine() {
