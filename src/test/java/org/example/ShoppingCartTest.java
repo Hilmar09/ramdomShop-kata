239f24696e89a6c95023fcf9c603df7fd9c57b22
Krisel1 <kriselur@gmail.com>
2024-06-19 14:42:31 +0200

    Create Testshopping

--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -5,14 +5,101 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
+    // Antiguedad cartas
     @Test
-    void calculatePriceForMagicCards_red() {
+    void calculatePrecioAntiguedadForMagicCards_red() {
+        // Arrange
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Lightning Bolt", null);
+        // Act
+        shoppingCart.addProduct(product);
+        // Assert
+        assertEquals(shoppingCart.getTotalPrice(), 1.75);
+
+    }
+
+    @Test
+    void calculatePrecioAntiguedadForMagicCards_blue() {
+        // Arrange
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Lightning Bolt", null);
+        // Act
+        shoppingCart.addProduct(product);
+        // Assert
+        assertEquals(product., 2.50);
+
+    }
+
+    @Test
+    void calculateAntiguedadForPets_Spiders() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "", null, "null", null);
 
         shoppingCart.addProduct(product);
 
+        assertEquals(product.getBasePrice(), 1.2);
+
+    }
+
+    @Test
+    void calculateAntiguedadForPets_RedSpiders() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "", null, "null", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), 11.60);
+
+    }
+
+    @Test
+    void calculateAntiguedadForPets_GoldSpiders() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "", null, "null", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), 12.60);
+
+    }
+    @Test
+    void calculateAntiguedadForPets_SpidersSttinky() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(8, null, true, "", null, "SpidersSttinky", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getSellPrice(), 9.60);
+
+    }
+
+    @Test
+    void calculateAntiguedadForPets_Spiders() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "", null, "null", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), 1.2);
+
+    }
+
+    @Test
+    void calculatePriceForMagicCards_red() {
+        // Arrange
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Lightning Bolt", null);
+        // Act
+        shoppingCart.addProduct(product);
+        // Assert
         assertEquals(shoppingCart.getTotalPrice(), 3.5);
 
     }
@@ -26,6 +113,55 @@ class ShoppingCartTest {
         shoppingCart.addProduct(product);
 
         assertEquals(shoppingCart.getTotalPrice(), 5.0);
+
+    }
+
+    @Test
+    void calculatePriceForMagicCards_green() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 4.40);
+
+    }
+
+    @Test
+    void calculatePriceForMagicCards_black() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 6.80);
+
+    }
+
+    // TODO cambiar lo default del código del color marron
+    @Test
+    void calculatePriceForMagicCards_brown() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "brown", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2.0);
+
+    }
+
+    @Test
+    void calculatePriceForMagicCards_blackLotus() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "null", null, "Magic: The Gathering - Black Lotus", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 40000.0);
 
     }
 
@@ -52,4 +188,30 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 50.0);
 
     }
+
+    @Test
+    void calculatePriceForPets_BlueFist() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "blue", null, "null", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), null);
+
+    }
+
+    @Test
+    void calculatePriceForPets_GoldFist() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, 5, false, "gold", null, "null", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(product.getBasePrice(), null);
+
+    }
+}
+
 }
